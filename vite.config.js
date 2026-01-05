8795d2213db26cac870bf7d0f823e71b1c277c52
jordan wall <jordanwall824@gmail.com>
2026-01-04 23:49:12 -0500

    Fix vite base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/thestoryofjordanwall/",   // 👈 IMPORTANT for GitHub Pages
+  base: "/",   // 👈 IMPORTANT for GitHub Pages
 });
 
